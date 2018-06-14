d84a9e6eba7a280ce38878ca3df86a548a3d383f
aziotclb <aziotclb@microsoft.com>
2018-06-14 10:40:08 -0700

    Sync Arduino libraries with latest Azure IoT SDK 1.0.45

--- src/AzureIoTProtocol_HTTP.h
+++ src/AzureIoTProtocol_HTTP.h
@@ -4,6 +4,6 @@
 #ifndef AZUREIOTPROTOCOLHTTP_H
 #define AZUREIOTPROTOCOLHTTP_H
 
-#define AzureIoTProtocolHTTPVersion "1.0.43"
+#define AzureIoTProtocolHTTPVersion "1.0.45"
 
 #endif //AZUREIOTPROTOCOLHTTP_H
